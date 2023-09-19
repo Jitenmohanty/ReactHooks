eebbfd4fd48d1cbd0b54fdee2476ff69aee09c46
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2023-09-19 19:27:16 +0530

    Here we add Router some better way and Use the concept of useParams

--- reactRouterDomProject/src/main.jsx
+++ reactRouterDomProject/src/main.jsx
@@ -1,32 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import Layout from "./Layout.jsx";
 import Home from "./Components/Home/Home.jsx";
 import About from "./Components/About/About.jsx";
 import Contact from "./Components/Contact/Contact";
+import User from "./Components/Users/User";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-      {
-        path: "contact",
-        element: <Contact />,
-      },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />,
+//       },
+//       {
+//         path: "about",
+//         element: <About />,
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />,
+//       },
+//     ],
+//   },
+// ]);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<Layout/>}>
+    <Route path="" element={<Home/>}/>
+    <Route path="about" element={<About/>}/>
+    <Route path="contact" element={<Contact/>}/>
+    <Route path="/user/:userid" element={<User/>}/>
+    {/* <Route path="/" element={<Home/>}/> */}
+  </Route>)
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
