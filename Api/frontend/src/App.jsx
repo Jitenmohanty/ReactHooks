ee29d14b19cd32b208d92be8c4be5fa864109014
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2023-10-12 20:39:53 +0530

    custom react query build

--- Api/frontend/src/App.jsx
+++ Api/frontend/src/App.jsx
@@ -1,19 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
-import { useEffect } from "react";
 
 function App() {
+  const [error, loading, products] = customReactQuery("/api/products");
+
+  if (error) {
+    return <h3>Something went Wrong.</h3>;
+  }
+
+  return (
+    <>
+      {loading ? (
+        <h3>Loading....</h3>
+      ) : (
+        <h3>Number of Products: {products.length}</h3>
+      )}
+   
+    </>
+  );
+}
+
+export default App;
+
+const customReactQuery = (urlPath) => {
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(true);
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    (async () => {
+    ;(async () => {
       try {
         setLoading(true);
         setError(false);
-        const response = await axios.get("/api/products");
+        const response = await axios.get(urlPath);
         console.log(response.data);
         setProducts(response.data);
         setLoading(false);
@@ -23,18 +43,5 @@ function App() {
       }
     })();
   }, []);
-
- 
-  if (error) {
-    return <h3>Something went Wrong.</h3>;
-  }
-
-  return (
-    <>
-      
-      {loading ? <h3>Loading....</h3>:  <h3>Number of Products: {products.length}</h3>}
-    </>
-  );
-}
-
-export default App;
+  return [error,loading,products];
+};
